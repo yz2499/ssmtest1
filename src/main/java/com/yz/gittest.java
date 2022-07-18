194ad79ebe75bb498f685ffe9ad6efa225fc730e
yz <1773761937@qq.com>
2022-07-18 18:35:07 +0800

    first commit

--- src/main/java/com/yz/gittest.java
+++ src/main/java/com/yz/gittest.java
@@ -2,7 +2,10 @@ package com.yz;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git0");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 
 }
